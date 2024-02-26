d553cb3e71eb95365c518f32e154a88694621d89
deepIify <kjm71712@naver.com>
2024-02-26 09:20:04 +0900

    consonantsAndGathers to private

--- src/main/kotlin/com/sushistack/linktree/utils/ArticleUtils.kt
+++ src/main/kotlin/com/sushistack/linktree/utils/ArticleUtils.kt
@@ -2,7 +2,7 @@ package com.sushistack.linktree.utils
 
 class ArticleUtils {
     companion object {
-        val consonantsAndGathers = listOf(
+        private val consonantsAndGathers = listOf(
             "ㄱ", "ㄴ", "ㄷ", "ㄹ", "ㅁ", "ㅂ", "ㅅ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ", "ㄲ", "ㄸ", "ㅃ", "ㅆ", "ㅉ",
             "ㅏ", "ㅑ", "ㅓ", "ㅕ", "ㅜ", "ㅠ", "ㅗ", "ㅛ", "ㅡ", "ㅣ", "ㅐ", "ㅔ", "ㅟ", "ㅢ", "ㅙ", "ㅝ", "ㅞ", "ㅒ", "ㅖ"
         )
