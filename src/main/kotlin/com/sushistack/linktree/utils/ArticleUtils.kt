a294c7e07e1cb11782ea4ab6db8ce161027103ab
deepIify <kjm71712@naver.com>
2024-02-26 10:03:11 +0900

    add spinSynonyms

--- src/main/kotlin/com/sushistack/linktree/utils/ArticleUtils.kt
+++ src/main/kotlin/com/sushistack/linktree/utils/ArticleUtils.kt
@@ -1,7 +1,13 @@
 package com.sushistack.linktree.utils
 
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import org.springframework.core.io.ClassPathResource
+import java.nio.charset.StandardCharsets
+
 class ArticleUtils {
     companion object {
+        private const val SYNONYMS_FILE_PATH = "synonyms.json"
         private val consonantsAndGathers = listOf(
             "ㄱ", "ㄴ", "ㄷ", "ㄹ", "ㅁ", "ㅂ", "ㅅ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ", "ㄲ", "ㄸ", "ㅃ", "ㅆ", "ㅉ",
             "ㅏ", "ㅑ", "ㅓ", "ㅕ", "ㅜ", "ㅠ", "ㅗ", "ㅛ", "ㅡ", "ㅣ", "ㅐ", "ㅔ", "ㅟ", "ㅢ", "ㅙ", "ㅝ", "ㅞ", "ㅒ", "ㅖ"
@@ -15,5 +21,25 @@ class ArticleUtils {
             return newContent
         }
 
+        fun spinSynonyms(content: String): String {
+            val synonyms = ClassPathResource(SYNONYMS_FILE_PATH)
+                .inputStream.readBytes()
+                .toString(StandardCharsets.UTF_8)
+                .let { Json.decodeFromString<Synonym>(it).synonyms }
+
+            return content.split(" ").joinToString(" ") { word ->
+                for (s in synonyms) {
+                    if (s.contains(word)) {
+                        return@joinToString s.random()
+                    }
+                }
+                return@joinToString word
+            }
+        }
     }
 }
+
+@Serializable
+data class Synonym (
+    val synonyms: List<List<String>>
+)
