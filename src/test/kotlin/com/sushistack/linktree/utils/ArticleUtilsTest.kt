a54bd752f06516e64715784f8e15e2e10c18ab5b
deepIify <kjm71712@naver.com>
2024-02-26 09:29:04 +0900

    add removeConsonantsAndGathers Test

---
+++ src/test/kotlin/com/sushistack/linktree/utils/ArticleUtilsTest.kt
@@ -0,0 +1,27 @@
+package com.sushistack.linktree.utils
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+class ArticleUtilsTest {
+
+    @ParameterizedTest(name = "[{index}] content = [{0}]")
+    @MethodSource("contentsProvider")
+    fun removeConsonantsAndGathersTest(content: String, expected: Boolean) {
+        // Then
+        val result = ArticleUtils.removeConsonantsAndGathers(content)
+        Assertions.assertThat(result == content).isEqualTo(expected)
+    }
+
+    companion object {
+        @JvmStatic
+        fun contentsProvider() = listOf(
+            Arguments.of("가나다라마", true),
+            Arguments.of("고고고ㄱ고고", false),
+            Arguments.of("ㅏㅏ카캄ㅁㅁ댜", false),
+            Arguments.of("ㅓㅓㅓ매대알ㄹ갹", false)
+        )
+    }
+}
