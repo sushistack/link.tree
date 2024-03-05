ff08d702ea5ef9f0917fa881423463d036096d9d
shushistack <kjm71712@naver.com>
2024-03-05 19:34:22 +0900

    snake to camel

--- src/main/kotlin/com/sushistack/linktree/entity/publisher/Comment.kt
+++ src/main/kotlin/com/sushistack/linktree/entity/publisher/Comment.kt
@@ -9,7 +9,7 @@ class Comment (
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "comment_seq", nullable = false)
-    val comment_seq: Long = 0,
+    val commentSeq: Long = 0,
 
     @Column(name = "post_url", nullable = false)
     val postUrl: String = ""
