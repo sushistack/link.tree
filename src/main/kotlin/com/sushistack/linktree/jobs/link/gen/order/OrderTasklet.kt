0f3d0c20fcedad36f7f6faec0c5ddf8a1dbe36d7
shushistack <kjm71712@naver.com>
2024-03-16 18:23:44 +0900

    add OrderTasklet

---
+++ src/main/kotlin/com/sushistack/linktree/jobs/link/gen/order/OrderTasklet.kt
@@ -0,0 +1,37 @@
+package com.sushistack.linktree.jobs.link.gen.order
+
+import com.sushistack.linktree.entity.order.Order
+import com.sushistack.linktree.entity.order.OrderType
+import com.sushistack.linktree.repository.order.OrderRepository
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.springframework.batch.core.StepContribution
+import org.springframework.batch.core.configuration.annotation.JobScope
+import org.springframework.batch.core.scope.context.ChunkContext
+import org.springframework.batch.core.step.tasklet.Tasklet
+import org.springframework.batch.repeat.RepeatStatus
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+
+@JobScope
+@Component
+class OrderTasklet(
+    @Value("#{jobParameters['orderType']}") private val orderType: String,
+    @Value("#{jobParameters['targetUrl']}") private val targetUrl: String,
+    @Value("#{jobParameters['customerName']}") private val customerName: String,
+    private val orderRepository: OrderRepository
+): Tasklet {
+
+    val log = KotlinLogging.logger {}
+
+    override fun execute(contribution: StepContribution, chunkContext: ChunkContext): RepeatStatus? {
+        val order = Order(
+            orderType = OrderType.valueOf(orderType),
+            targetUrl = targetUrl,
+            customerName = customerName
+        )
+        orderRepository.save(order)
+        log.info { "Saved Order := [${order}]" }
+
+        return RepeatStatus.FINISHED
+    }
+}
