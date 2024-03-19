b9a597c9d40a882c4c4ed6fb2e19a32b60645281
shushistack <kjm71712@naver.com>
2024-03-19 18:11:34 +0900

    remove qualifying

--- src/main/kotlin/com/sushistack/linktree/JobRunner.kt
+++ src/main/kotlin/com/sushistack/linktree/JobRunner.kt
@@ -5,7 +5,6 @@ import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.JobParametersBuilder
 import org.springframework.batch.core.launch.JobLauncher
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.CommandLineRunner
 import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
@@ -16,7 +15,7 @@ class JobRunner {
     private val log = KotlinLogging.logger {}
 
     @Bean
-    fun runJob(jobLauncher: JobLauncher, @Qualifier("linkGenerationJob") job: Job): CommandLineRunner {
+    fun runJob(jobLauncher: JobLauncher, job: Job): CommandLineRunner {
         return CommandLineRunner {
             val jobParameters = JobParametersBuilder()
                 .addLong("runTime", System.currentTimeMillis())
