1bfbc76c33c70744726913bf478ba092a5971957
shushistack <kjm71712@naver.com>
2024-03-19 21:44:21 +0900

    change StaticWebpageRepositoryImpl

--- src/main/kotlin/com/sushistack/linktree/repository/publisher/StaticWebpageRepositoryImpl.kt
+++ src/main/kotlin/com/sushistack/linktree/repository/publisher/StaticWebpageRepositoryImpl.kt
@@ -2,14 +2,16 @@ package com.sushistack.linktree.repository.publisher
 
 import com.querydsl.jpa.impl.JPAQueryFactory
 import com.sushistack.linktree.entity.publisher.QStaticWebpage.staticWebpage
+import com.sushistack.linktree.entity.publisher.ServiceProviderType
 import com.sushistack.linktree.entity.publisher.StaticWebpage
 
 class StaticWebpageRepositoryImpl(private val queryFactory: JPAQueryFactory): StaticWebpageRepositoryCustom {
 
-    override fun findStaticWebpagesByOrderByUsedCountAscLimit(limit: Long): List<StaticWebpage> =
+    override fun findStaticWebpagesProviderTypeByOrderByUsedCountAscLimit(providerType: ServiceProviderType, limit: Long): List<StaticWebpage> =
         queryFactory
             .selectFrom(staticWebpage)
             .orderBy(staticWebpage.usedCount.asc())
+            .where(staticWebpage.providerType.eq(providerType))
             .limit(limit)
             .fetch()
 
