d8bf7da8b10c816799aa1786d06aaee77d5fa59d
shushistack <kjm71712@naver.com>
2024-03-19 19:20:44 +0900

    add OrderRepository Test

---
+++ src/test/kotlin/com/sushistack/linktree/repository/order/OrderRepositoryTest.kt
@@ -0,0 +1,31 @@
+package com.sushistack.linktree.repository.order
+
+import com.sushistack.linktree.entity.order.Order
+import jakarta.persistence.EntityManager
+import jakarta.transaction.Transactional
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+@Transactional
+class OrderRepositoryTest {
+
+    @Autowired
+    lateinit var entityManager: EntityManager
+    @Autowired
+    lateinit var orderRepository: OrderRepository
+
+    @Test
+    fun saveTest() {
+        // Given
+        Order().let { entityManager.persist(it) }
+        entityManager.flush()
+        entityManager.clear()
+
+        // Then
+        val orders = orderRepository.findAll()
+        Assertions.assertThat(orders.size).isEqualTo(1)
+    }
+}
