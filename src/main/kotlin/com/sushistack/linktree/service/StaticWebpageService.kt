8aaf1304aba75ed2cb8c1ade33f18292468c73cb
shushistack <kjm71712@naver.com>
2024-03-21 18:40:41 +0900

    change StaticWebpageService

--- src/main/kotlin/com/sushistack/linktree/service/StaticWebpageService.kt
+++ src/main/kotlin/com/sushistack/linktree/service/StaticWebpageService.kt
@@ -1,5 +1,6 @@
 package com.sushistack.linktree.service
 
+import com.sushistack.linktree.entity.publisher.ServiceProviderType
 import com.sushistack.linktree.entity.publisher.StaticWebpage
 import com.sushistack.linktree.repository.publisher.StaticWebpageRepository
 import org.springframework.stereotype.Service
@@ -7,7 +8,7 @@ import org.springframework.stereotype.Service
 @Service
 class StaticWebpageService(private val staticWebpageRepository: StaticWebpageRepository) {
 
-    fun findStaticWebpages(limit: Long): List<StaticWebpage> =
-        staticWebpageRepository.findStaticWebpagesByOrderByUsedCountAscLimit(limit)
+    fun findStaticWebpagesByProviderType(providerType: ServiceProviderType, limit: Long): List<StaticWebpage> =
+        staticWebpageRepository.findStaticWebpagesProviderTypeByOrderByUsedCountAscLimit(providerType = providerType, limit = limit)
 
 }
