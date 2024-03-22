7c78013fee0137ac3459a249629b407a897c919e
shushistack <kjm71712@naver.com>
2024-03-22 19:41:22 +0900

    add implments to LinkNodeRepository

--- src/main/kotlin/com/sushistack/linktree/repository/link/LinkNodeRepository.kt
+++ src/main/kotlin/com/sushistack/linktree/repository/link/LinkNodeRepository.kt
@@ -3,4 +3,4 @@ package com.sushistack.linktree.repository.link
 import com.sushistack.linktree.entity.link.LinkNode
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface LinkNodeRepository : JpaRepository<LinkNode, Long>
+interface LinkNodeRepository : JpaRepository<LinkNode, Long>, LinkNodeRepositoryCustom
