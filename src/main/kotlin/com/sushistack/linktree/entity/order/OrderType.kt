12e4d91cb7b4bd9cb9f714fd4a793e6bef1df3d5
shushistack <kjm71712@naver.com>
2024-03-25 18:05:11 +0900

    add link count of orderTypes

--- src/main/kotlin/com/sushistack/linktree/entity/order/OrderType.kt
+++ src/main/kotlin/com/sushistack/linktree/entity/order/OrderType.kt
@@ -1,5 +1,5 @@
 package com.sushistack.linktree.entity.order
 
-enum class OrderType {
-    STANDARD, DELUXE, PREMIUM, UNKNOWN
+enum class OrderType(val linkCount: Int) {
+    STANDARD(10), DELUXE(40), PREMIUM(60), UNKNOWN(0)
 }
