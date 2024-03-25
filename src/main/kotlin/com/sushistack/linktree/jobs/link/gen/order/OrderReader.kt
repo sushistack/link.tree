788a7ecc4c102c8d2e9a29a6cc81f7ce40d12765
shushistack <kjm71712@naver.com>
2024-03-25 20:16:23 +0900

    add OrderReader

---
+++ src/main/kotlin/com/sushistack/linktree/jobs/link/gen/order/OrderReader.kt
@@ -0,0 +1,28 @@
+package com.sushistack.linktree.jobs.link.gen.order
+
+import com.sushistack.linktree.entity.order.Order
+import com.sushistack.linktree.entity.order.OrderStatus
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.springframework.batch.core.configuration.annotation.JobScope
+import org.springframework.batch.item.ItemReader
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+
+@JobScope
+@Component
+class OrderReader: ItemReader<Order> {
+    private val log = KotlinLogging.logger {}
+    private var processed = false
+
+    @Value("#{jobExecutionContext['order']}")
+    lateinit var order: Order
+
+    override fun read(): Order? {
+        return if (!processed) {
+            processed = true
+            order.orderStatus = OrderStatus.STAGED
+            log.info { "Read ${order}" }
+            order
+        } else null
+    }
+}
