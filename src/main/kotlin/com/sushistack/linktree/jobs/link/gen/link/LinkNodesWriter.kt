921f8e955cc5c1530ea7327b79f5aa707af2a63f
shushistack <kjm71712@naver.com>
2024-03-26 20:11:44 +0900

    add LinkNodesWriter

---
+++ src/main/kotlin/com/sushistack/linktree/jobs/link/gen/link/LinkNodesWriter.kt
@@ -0,0 +1,18 @@
+package com.sushistack.linktree.jobs.link.gen.link
+
+import com.sushistack.linktree.entity.link.LinkNode
+import com.sushistack.linktree.service.LinkNodeService
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.springframework.batch.item.Chunk
+import org.springframework.batch.item.ItemWriter
+import org.springframework.stereotype.Component
+
+@Component
+class LinkNodesWriter(private val linkNodeService: LinkNodeService): ItemWriter<List<LinkNode>> {
+    val log = KotlinLogging.logger {}
+
+    override fun write(chunk: Chunk<out List<LinkNode>>) {
+         chunk.forEach { linkNodes -> linkNodeService.createLinkNodes(linkNodes) }
+        log.info { "Successfully created link nodes" }
+    }
+}
