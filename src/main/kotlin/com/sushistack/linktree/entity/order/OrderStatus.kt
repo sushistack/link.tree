d0ab486f74c621340332544c0e6023a8c0c8a9d2
shushistack <kjm71712@naver.com>
2024-04-09 17:53:10 +0900

    add next function on order status

--- src/main/kotlin/com/sushistack/linktree/entity/order/OrderStatus.kt
+++ src/main/kotlin/com/sushistack/linktree/entity/order/OrderStatus.kt
@@ -1,5 +1,17 @@
 package com.sushistack.linktree.entity.order
 
 enum class OrderStatus(val tier: Int) {
-    READY(0), PROCESSING_1(1), PROCESSING_2(2), PROCESSING_3(3), DONE(4)
+    READY(0), PROCESSING_1(1), PROCESSING_2(2), PROCESSING_3(3), DONE(4);
+
+    companion object {
+        fun next(status: OrderStatus): OrderStatus {
+            return when (status) {
+                READY -> PROCESSING_1
+                PROCESSING_1 -> PROCESSING_2
+                PROCESSING_2 -> PROCESSING_3
+                PROCESSING_3 -> DONE
+                DONE -> DONE
+            }
+        }
+    }
 }
---
+++ src/test/kotlin/com/sushistack/linktree/service/CommentServiceTest.kt
@@ -0,0 +1,12 @@
+package com.sushistack.linktree.service
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class CommentServiceTest {
+
+    @Test
+    fun findByOrderByUsedCountLimit() {
+    }
+}
