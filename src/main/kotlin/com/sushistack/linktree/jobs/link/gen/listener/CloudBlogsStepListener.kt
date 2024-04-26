176409744533ea972470f7f967d9cdc597e275cd
shushistack <kjm71712@naver.com>
2024-04-26 19:54:21 +0900

    add listeners

---
+++ src/main/kotlin/com/sushistack/linktree/jobs/link/gen/listener/CloudBlogsStepListener.kt
@@ -0,0 +1,26 @@
+package com.sushistack.linktree.jobs.link.gen.listener
+
+import com.sushistack.linktree.entity.order.Order
+import com.sushistack.linktree.entity.order.OrderStatus
+import io.github.oshai.kotlinlogging.KotlinLogging
+import org.springframework.batch.core.ExitStatus
+import org.springframework.batch.core.StepExecution
+import org.springframework.batch.core.StepExecutionListener
+import org.springframework.batch.core.configuration.annotation.JobScope
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+
+@JobScope
+@Component
+class CloudBlogsStepListener: StepExecutionListener {
+    private val log = KotlinLogging.logger {}
+
+    @Value("#{jobExecutionContext['order']}")
+    lateinit var order: Order
+
+    override fun afterStep(stepExecution: StepExecution): ExitStatus {
+        order.orderStatus = OrderStatus.next(order.orderStatus)
+        log.info { "${stepExecution.stepName} is Completed, Order(${order.orderStatus})" }
+        return stepExecution.exitStatus;
+    }
+}
