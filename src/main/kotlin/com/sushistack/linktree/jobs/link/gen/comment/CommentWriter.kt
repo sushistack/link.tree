e74862b4e0ae3edec75420bfbd7dbf19b42549c9
shushistack <kjm71712@naver.com>
2024-04-30 19:14:31 +0900

    remove unused imports

--- src/main/kotlin/com/sushistack/linktree/jobs/link/gen/comment/CommentWriter.kt
+++ src/main/kotlin/com/sushistack/linktree/jobs/link/gen/comment/CommentWriter.kt
@@ -1,14 +1,10 @@
 package com.sushistack.linktree.jobs.link.gen.comment
 
 import com.sushistack.linktree.entity.link.LinkNode
-import com.sushistack.linktree.entity.order.Order
-import com.sushistack.linktree.entity.order.OrderStatus
 import com.sushistack.linktree.service.LinkNodeService
 import io.github.oshai.kotlinlogging.KotlinLogging
-import org.springframework.batch.core.configuration.annotation.JobScope
 import org.springframework.batch.item.Chunk
 import org.springframework.batch.item.ItemWriter
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 @Component
