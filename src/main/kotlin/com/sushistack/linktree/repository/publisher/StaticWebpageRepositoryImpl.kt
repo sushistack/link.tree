ec338b7866cd3c2024baefc182655905da2ea2e7
shushistack <kjm71712@naver.com>
2024-05-01 19:24:54 +0900

    query tunning of staticwebpagerepository

--- src/main/kotlin/com/sushistack/linktree/repository/publisher/StaticWebpageRepositoryImpl.kt
+++ src/main/kotlin/com/sushistack/linktree/repository/publisher/StaticWebpageRepositoryImpl.kt
@@ -1,6 +1,9 @@
 package com.sushistack.linktree.repository.publisher
 
 import com.querydsl.jpa.impl.JPAQueryFactory
+import com.sushistack.linktree.entity.git.QGitAccount
+import com.sushistack.linktree.entity.git.QGitAccount.gitAccount
+import com.sushistack.linktree.entity.git.QGitRepository.gitRepository
 import com.sushistack.linktree.entity.publisher.QStaticWebpage.staticWebpage
 import com.sushistack.linktree.entity.publisher.ServiceProviderType
 import com.sushistack.linktree.entity.publisher.StaticWebpage
@@ -10,6 +13,8 @@ class StaticWebpageRepositoryImpl(private val queryFactory: JPAQueryFactory): St
     override fun findStaticWebpagesProviderTypeByOrderByUsedCountAscLimit(providerType: ServiceProviderType, limit: Long): List<StaticWebpage> =
         queryFactory
             .selectFrom(staticWebpage)
+            .join(staticWebpage.repository, gitRepository).fetchJoin()
+            .join(gitRepository.gitAccount, gitAccount).fetchJoin()
             .orderBy(staticWebpage.usedCount.asc())
             .where(staticWebpage.providerType.eq(providerType))
             .limit(limit)
