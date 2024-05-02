14f217effeafb6522a1e5e45208bdd71268e830a
shushistack <kjm71712@naver.com>
2024-05-02 18:30:11 +0900

    query tunning of linknoderepository and reader

--- src/main/kotlin/com/sushistack/linktree/repository/link/LinkNodeRepositoryImpl.kt
+++ src/main/kotlin/com/sushistack/linktree/repository/link/LinkNodeRepositoryImpl.kt
@@ -1,6 +1,8 @@
 package com.sushistack.linktree.repository.link
 
 import com.querydsl.jpa.impl.JPAQueryFactory
+import com.sushistack.linktree.entity.git.QGitAccount
+import com.sushistack.linktree.entity.git.QGitRepository.gitRepository
 import com.sushistack.linktree.entity.link.LinkNode
 import com.sushistack.linktree.entity.link.QLinkNode.linkNode
 import com.sushistack.linktree.entity.order.Order
@@ -10,8 +12,13 @@ class LinkNodeRepositoryImpl(private val queryFactory: JPAQueryFactory): LinkNod
     override fun findByOrderAndTier(order: Order, tier: Int): List<LinkNode> =
         queryFactory
             .selectFrom(linkNode)
-            .innerJoin(QOrder.order)
-            .on(linkNode.order.orderSeq.eq(QOrder.order.orderSeq).and(QOrder.order.orderSeq.eq(order.orderSeq)))
-            .where(linkNode.tier.eq(tier))
+            .join(linkNode.order, QOrder.order).fetchJoin()
+            .join(linkNode.repository, gitRepository).fetchJoin()
+            .join(gitRepository.gitAccount, QGitAccount.gitAccount).fetchJoin()
+            .where(
+                linkNode.tier.eq(tier)
+                    .and(linkNode.order.orderSeq.eq(QOrder.order.orderSeq))
+                    .and(QOrder.order.orderSeq.eq(order.orderSeq))
+            )
             .fetch()
 }
