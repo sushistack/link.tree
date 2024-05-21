172b130cb6a90f8a0a65f1edda2837f1eec53536
shushistack <kjm71712@naver.com>
2024-05-21 21:12:51 +0900

    modify logics in order to linknodeprocessor

--- src/main/kotlin/com/sushistack/linktree/jobs/link/gen/order/OrderToLinkNodesProcessor.kt
+++ src/main/kotlin/com/sushistack/linktree/jobs/link/gen/order/OrderToLinkNodesProcessor.kt
@@ -6,7 +6,6 @@ import com.sushistack.linktree.entity.order.Order
 import com.sushistack.linktree.entity.publisher.ServiceProviderType
 import com.sushistack.linktree.service.PostService
 import com.sushistack.linktree.service.StaticWebpageService
-import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.batch.item.ItemProcessor
 import org.springframework.stereotype.Component
 
@@ -15,15 +14,20 @@ class OrderToLinkNodesProcessor(
     private val postService: PostService,
     private val staticWebpageService: StaticWebpageService
 ): ItemProcessor<Order, List<LinkNode>> {
-    private val log = KotlinLogging.logger {}
 
     override fun process(order: Order): List<LinkNode> {
         val webpages = staticWebpageService.findStaticWebpagesByProviderType(ServiceProviderType.PRIVATE_BLOG_NETWORK, order.orderType.linkCount.toLong())
 
         return webpages.map { webpage ->
-            val post = Post(webpage = webpage)
+            val post = Post(filePath = "life/test.md", webpage = webpage)
             postService.createPost(post)
-            LinkNode(order = order, repository = webpage.repository, tier = order.orderStatus.tier + 1, parentNodeSeq = order.orderSeq)
+            LinkNode(
+                order = order,
+                url = webpage.getPostUrl(post),
+                repository = webpage.repository,
+                tier = order.orderStatus.tier + 1,
+                parentNodeSeq = order.orderSeq
+            )
         }
     }
 }
