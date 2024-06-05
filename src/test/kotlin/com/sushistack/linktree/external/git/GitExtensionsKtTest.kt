594cdc590f5aee2a91cb610a1b1ea200eafb0f16
shushistack <kjm71712@naver.com>
2024-06-05 18:18:31 +0900

    add jgit

---
+++ src/test/kotlin/com/sushistack/linktree/external/git/GitExtensionsKtTest.kt
@@ -0,0 +1,19 @@
+package com.sushistack.linktree.external.git.client
+
+import com.sushistack.linktree.external.git.*
+import org.eclipse.jgit.api.Git
+import org.junit.jupiter.api.Test
+
+class GitExtensionsKtTest {
+
+    @Test
+    fun gitOpenTest() {
+        // Given
+
+        // When
+        Git.open()
+
+        // Then
+    }
+
+}
