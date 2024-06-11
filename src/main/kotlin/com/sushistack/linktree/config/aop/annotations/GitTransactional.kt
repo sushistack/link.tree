c982b5b989925fb7c7afe0c5b8afa815851f7753
sushistack <kjm71712@naver.com>
2024-06-11 19:31:31 +0900

    add git logics and posting

--- src/main/kotlin/com/sushistack/linktree/config/aop/annotations/GitTransactional.kt
+++
@@ -1,8 +0,0 @@
-package com.sushistack.linktree.config.aop.annotations
-
-import org.springframework.transaction.annotation.Transactional
-
-@Transactional
-@Retention(AnnotationRetention.RUNTIME)
-@Target(AnnotationTarget.FUNCTION)
-annotation class GitTransactional
---
+++ src/test/kotlin/com/sushistack/linktree/utils/CsvUtilsTest.kt
@@ -0,0 +1,5 @@
+package com.sushistack.linktree.utils
+
+import org.junit.jupiter.api.Assertions.*
+
+class CsvUtilsTest
