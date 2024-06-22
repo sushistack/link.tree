4db78361c1bddc6cbdb3811daae557630139b295
sushistack <kjm71712@naver.com>
2024-06-22 18:51:28 +0900

    add ftp settings

---
+++ src/main/kotlin/com/sushistack/linktree/external/ftp/config/FTPConfig.kt
@@ -0,0 +1,20 @@
+package com.sushistack.linktree.external.ftp.config
+
+import org.apache.commons.net.ftp.FTPClient.PASSIVE_LOCAL_DATA_CONNECTION_MODE
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.integration.ftp.session.DefaultFtpSessionFactory
+
+@Configuration
+class FTPConfig(private val ftp: FTPProperties) {
+
+    @Bean
+    fun ftpSessionFactory(): DefaultFtpSessionFactory =
+        DefaultFtpSessionFactory().apply {
+            setHost(ftp.host)
+            setPort(ftp.port)
+            setUsername(ftp.username)
+            setPassword(ftp.password)
+            setClientMode(PASSIVE_LOCAL_DATA_CONNECTION_MODE)
+        }
+}
