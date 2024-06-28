792e0edac562ad42ae80c0afc7e34c2b658ce737
sushistack <kjm71712@naver.com>
2024-06-28 19:24:45 +0900

    update job parameter logics

--- src/main/kotlin/com/sushistack/linktree/JobRunner.kt
+++ src/main/kotlin/com/sushistack/linktree/JobRunner.kt
@@ -8,24 +8,33 @@ import org.springframework.batch.core.Job
 import org.springframework.batch.core.JobParametersBuilder
 import org.springframework.batch.core.launch.JobLauncher
 import org.springframework.boot.CommandLineRunner
-import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
 
 @Component
-class JobRunner {
+class JobRunner(private val jobLauncher: JobLauncher, private val job: Job): CommandLineRunner {
     private val log = KotlinLogging.logger {}
 
-    @Bean
-    fun runJob(jobLauncher: JobLauncher, job: Job) = CommandLineRunner {
+    override fun run(vararg args: String?) {
+        val paramMap = getJobParameterMap(args)
+
+        log.info { "paramMap: $paramMap" }
+
         val jobParameters = JobParametersBuilder()
-            .addString("orderType", OrderType.DELUXE.name)
-            .addString("targetUrl", "https://test.com")
-            .addString("customerName", "고객명14")
-            .addString("anchorTexts", Json.encodeToString(listOf("감자", "감자맨")))
-            .addString("keywords", Json.encodeToString(listOf("감자의 효능")))
+            .addString("orderType", paramMap["orderType"] ?: OrderType.DELUXE.name)
+            .addString("targetUrl", paramMap["targetUrl"] ?: "https://test.com")
+            .addString("customerName", paramMap["customerName"] ?: "고객명14")
+            .addString("anchorTexts", paramMap["anchorTexts"] ?: Json.encodeToString(listOf("감자", "감자맨")))
+            .addString("keywords", paramMap["keywords"] ?: Json.encodeToString(listOf("감자의 효능")))
             .toJobParameters()
 
-         val jobExecution = jobLauncher.run(job, jobParameters)
-         log.info { "Job Status: ${jobExecution.status}" }
+        val jobExecution = jobLauncher.run(job, jobParameters)
+        log.info { "Job Status: ${jobExecution.status}" }
     }
+
+    private fun getJobParameterMap(args: Array<out String?>) =
+        args.filterNotNull()
+            .associate {
+                val (key, value) = it.split("=")
+                key to value
+            }.toMap()
 }
