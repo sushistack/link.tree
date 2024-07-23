65a6744ba848e0c06f4ffe80ac8d2fa01ba23718
sushistack <kjm71712@naver.com>
2024-07-23 22:21:14 +0900

    enhance slack noti

--- src/main/kotlin/com/sushistack/linktree/service/SlackNotificationService.kt
+++ src/main/kotlin/com/sushistack/linktree/service/SlackNotificationService.kt
@@ -15,6 +15,10 @@ class SlackNotificationService(
     private val methodsClient: MethodsClient,
     private val slackChannel: String
 ) {
+    companion object {
+        private val symbol: (Boolean) -> String = { if (it) ":green_circle:" else ":red_circle:" }
+    }
+
     private val log = KotlinLogging.logger {}
 
     fun send(message: String, channel: String = slackChannel) =
@@ -52,20 +56,30 @@ class SlackNotificationService(
 
         blocks.add(Blocks.section { it.text(MarkdownTextObject.builder().text("*Post Validation List(Tier 1)*").build()) })
 
-        blocks.add(Blocks.section { it.fields(linkMap1.map { entry -> MarkdownTextObject.builder().text("*Code(${entry.key}):* ${entry.value.size}").build() }) })
+        blocks.add(Blocks.section {
+            it.fields(linkMap1.map { entry ->
+                MarkdownTextObject.builder().text("*Code(${entry.key} ${symbol(entry.value.isEmpty())}):* ${entry.value.size}").build()
+            })
+        })
 
         blocks.add(Blocks.divider())
 
         blocks.add(Blocks.section { it.text(MarkdownTextObject.builder().text("*Post Validation List(Tier 2)*").build()) })
-        blocks.add(Blocks.section { it.fields(linkMap2.map { entry -> MarkdownTextObject.builder().text("*Code(${entry.key}):* ${entry.value.size}").build() }) })
+        blocks.add(Blocks.section {
+            it.fields(linkMap2.map { entry ->
+                val symbol = if (entry.value.isEmpty()) ":green_circle:" else ":red_circle:"
+                MarkdownTextObject.builder().text("$symbol *Code(${entry.key} ${symbol(entry.value.isEmpty())}):* ${entry.value.size}").build()
+            })
+        })
 
         send(blocks, slackChannel)
     }
 
     fun sendJobDetail(jobDetail: JobDetail, stepDetails: List<StepDetail>) {
+        val isCompleted = jobDetail.status != BatchStatus.COMPLETED.name
+
         val blocks = mutableListOf<LayoutBlock>()
         blocks.add(Blocks.section { it.text(MarkdownTextObject.builder().text("${getDescription(jobDetail.name)} 결과").build()) })
-
         blocks.add(Blocks.divider())
 
         blocks.add(
@@ -73,7 +87,7 @@ class SlackNotificationService(
                 it.fields(
                     listOf(
                         MarkdownTextObject.builder().text("*Job Name:*\n${jobDetail.name}").build(),
-                        MarkdownTextObject.builder().text("*Status:*\n${jobDetail.status}").build(),
+                        MarkdownTextObject.builder().text("*Status:*\n${symbol(isCompleted)} ${jobDetail.status}").build(),
                         MarkdownTextObject.builder().text("*Start Time:*\n${jobDetail.startTime}").build(),
                         MarkdownTextObject.builder().text("*End Time:*\n${jobDetail.endTime}").build()
                     )
@@ -81,16 +95,16 @@ class SlackNotificationService(
             }
         )
 
-        if (jobDetail.status != BatchStatus.COMPLETED.name) {
+        if (isCompleted) {
             blocks.add(Blocks.section { it.text(MarkdownTextObject.builder().text("*Step Details:*").build()) })
-
             stepDetails.forEach { step ->
+                val isStepCompleted = step.status == BatchStatus.COMPLETED.name
                 blocks.add(
                     Blocks.section {
                         it.fields(
                             listOf(
                                 MarkdownTextObject.builder().text("*Step Name:*\n${step.name}").build(),
-                                MarkdownTextObject.builder().text("*Status:*\n${step.status}").build()
+                                MarkdownTextObject.builder().text("*Status:*\n${symbol(isStepCompleted)} ${step.status}").build()
                             )
                         )
                     }
